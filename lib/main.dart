08b55d8e57d60d33b94b4257817d321775a4b02f
andrehaueisen <andrehaueisen@gmail.com>
2019-03-06 15:03:38 -0300

    reduced file size

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:flutter/material.dart';
 import 'package:aeyrium_sensor/aeyrium_sensor.dart';
 import 'package:flutter/services.dart';
 
@@ -10,7 +10,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Compass(),
@@ -19,105 +18,87 @@ class MyApp extends StatelessWidget {
 }
 
 class Compass extends StatefulWidget {
-  @override
   _CompassState createState() => _CompassState();
 }
 
 class _CompassState extends State<Compass> with TickerProviderStateMixin {
+
   AnimationController cntller;
   var subs;
   var height = 0.0;
 
-  @override
   void initState() {
     cntller = AnimationController(vsync: this, lowerBound: -3, upperBound: 3);
 
     subs = AeyriumSensor.sensorEvents.listen((SensorEvent event) {
-      cntller.animateTo(event.roll, duration: Duration(milliseconds: 100));
+      cntller.animateTo(event.roll, duration: Duration(milliseconds: 50));
       height = cntller.value < -2 ? 36 : 0;
     });
 
     super.initState();
   }
 
-  @override
   void dispose() {
     cntller.dispose();
     subs.dispose();
     super.dispose();
   }
 
-  @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Image(
-              image: AssetImage(
-                'res/bg.jpeg',
-              ),
-              fit: BoxFit.fitHeight),
-          Center(
-            child: Container(
+        body: Stack(fit: StackFit.expand, children: [
+      Image(
+          image: AssetImage(
+            'res/bg.jpeg',
+          ),
+          fit: BoxFit.fitHeight),
+      Center(
+          child: Container(
               height: width,
               width: width,
-              padding: const EdgeInsets.all(48),
+              padding: EdgeInsets.all(48),
               child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage('res/comp.jpeg')),
-                  shape: BoxShape.circle,
-                ),
-                child: AnimatedBuilder(
-                  animation: cntller,
-                  builder: (_, __) {
-                    return Transform(
-                      alignment: Alignment.center,
-                      transform: Matrix4.rotationZ(cntller.value),
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          CustomPaint(
-                            painter: StarPainter(),
-                            size: Size.square(width),
-                          ),
-                          AnimatedSize(
-                            vsync: this,
-                            duration: Duration(seconds: 4),
-                            child: Image(
-                              image: AssetImage('res/logo.png'),
-                              height: height,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage('res/comp.jpeg')),
+                    shape: BoxShape.circle,
+                  ),
+                  child: AnimatedBuilder(
+                      animation: cntller,
+                      builder: (_, __) => Transform(
+                          alignment: Alignment.center,
+                          transform: Matrix4.rotationZ(cntller.value),
+                          child: Stack(alignment: Alignment.center, children: [
+                            CustomPaint(
+                              painter: StarPainter(),
+                              size: Size.square(width),
                             ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+                            AnimatedSize(
+                              vsync: this,
+                              duration: Duration(seconds: 9),
+                              child: Image(
+                                image: AssetImage('res/logo.png'),
+                                height: height,
+                              ),
+                            )
+                          ]))))))]));
   }
 }
 
 class StarPainter extends CustomPainter {
-  Paint pDark;
+  final Paint pDark = Paint();
 
   final titles = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
-  final txtPtr = TextPainter(textAlign: TextAlign.center, textDirection: TextDirection.ltr);
-  final txtStl = TextStyle(color: Color(0xFFEDC394), fontSize: 24, fontFamily: 'Calli', fontWeight: FontWeight.bold);
+  final pTxt = TextPainter(textDirection: TextDirection.ltr);
+  final stlTxt = TextStyle(color: Color(0xFFEDC394), fontSize: 24, fontFamily: 'Calli', fontWeight: FontWeight.bold);
 
   StarPainter() {
-    pDark = Paint();
-    pDark.color = Color(0XCC502814);
-    pDark.style = PaintingStyle.fill;
-    pDark.maskFilter = MaskFilter.blur(BlurStyle.solid, 2);
-    pDark.blendMode = BlendMode.colorBurn;
+    pDark
+      ..color = Color(0XCC502814)
+      ..style = PaintingStyle.fill
+      ..blendMode = BlendMode.colorBurn
+      ..maskFilter = MaskFilter.blur(BlurStyle.solid, 2);
   }
 
   @override
@@ -125,23 +106,22 @@ class StarPainter extends CustomPainter {
     cavs.save();
     cavs.translate(sze.width / 2, sze.height / 2);
 
-    var pth1 = Path();
-    
+    final pth1 = Path();
     final double sizeStep = 40 / 2;
 
     for (int i = 0; i < 8; i++) {
       pth1.moveTo(0, -30);
       pth1.lineTo(-sizeStep, -50);
-      pth1.lineTo(0, - sze.height / 2 + 25);
+      pth1.lineTo(0, -sze.height / 2 + 25);
       pth1.lineTo(sizeStep, -50);
       pth1.close();
 
       cavs.save();
       cavs.translate(0, -sze.height / 2);
 
-      txtPtr.text = TextSpan(style: txtStl, text: titles[i]);
-      txtPtr.layout();
-      txtPtr.paint(cavs, Offset(-txtPtr.width / 2, -(txtPtr.height / 2) - 18));
+      pTxt.text = TextSpan(style: stlTxt, text: titles[i]);
+      pTxt.layout();
+      pTxt.paint(cavs, Offset(-pTxt.width / 2, -(pTxt.height / 2) - 18));
 
       cavs.restore();
 
